3c6d0ee5125a8d753b876bcbdde92da8b72326d4
Lukutv04 <lukas.eriksson1@student.nbi-handelsakademin.se>
2025-11-09 16:11:15 +0100

    lagt till listor

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using App;
+
+/*
+
+- Logga in med uppgifter om ligger sparade i en fil
+- Se en lista av alla rum som har gäster just nu
+- Se en lista av alla lediga rum
+- Boka in en gäst i ett ledigt rum
+- Checka ut en gäst från ett upptaget rum
+- Markera att ett rum , temporärt, inte är tillgängligt
+
+*/
+
+List<Room> rooms = new List<Room>();
+List<User> users = new List<User>();
+
+User? loggedInUser = null;
+
+
+
+
+
