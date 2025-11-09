ddea5578d8b93b8229f44554c2db3f35e014a71a
Lukutv04 <lukas.eriksson1@student.nbi-handelsakademin.se>
2025-11-09 17:00:03 +0100

    lagt till namn och lösenord i user filen

---
+++ User.cs
@@ -0,0 +1,15 @@
+namespace App;
+
+public class User
+
+{
+    public string name;
+    public string password;
+
+    public User(string Name, string Password)
+
+    {
+        name = Name;
+        password = Password;
+    }
+}
