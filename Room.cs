2365c9e702470533cb7d3ee2ac1cb7e90159365c
Lukutv04 <lukas.eriksson1@student.nbi-handelsakademin.se>
2025-11-09 21:36:14 +0100

    har gjort klart Room klassen samt lagt till enum för rumsstatus

--- Room.cs
+++ Room.cs
@@ -3,5 +3,22 @@ namespace App;
 public class Room
 
 {
-    
+    public string roomnumber;
+    public string user;
+    public string status;
+
+    public Room(string Roomnumber, string User, string Status)
+    {
+        roomnumber = Roomnumber;
+        user = User;
+        status = Status;
+
+    }
+}
+
+public enum status
+{
+    available,
+    occupied,
+    cleanup,
 }
